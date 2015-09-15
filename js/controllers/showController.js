06f85882cc7a809d2e494ba80ef8c2ec4ceda338
Mostafa Habib <MostafaSHabib@gmail.com>
2015-09-15 00:45:40 -0400

    added a lot of weird stuff

--- js/controllers/showController.js
+++ js/controllers/showController.js
@@ -3,10 +3,12 @@ app.controller('showController', ['$http', '$scope', function($http, $scope) {
 
 
   //get request
-  $http.jsonp('https://archive.org/advancedsearch.php?q=BrothersPast&fl%5B%5D=identifier,title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=50&page=1&output=json&callback=JSON_CALLBACK')
+  $http.jsonp('https://archive.org/advancedsearch.php?q=BrothersPast&fl%5B%5D=identifier,title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=5&page=1&output=json&callback=JSON_CALLBACK')
   .success(function(data) {
-    console.log("success with AJAX request");
-    $scope.stuff = data;
+
+    $scope.showList = data;
+    console.log(data);
+
   })
   .error(function(data) {
     console.log("error with AJAX request");
---
+++ js/directives/showDirective.js
@@ -0,0 +1,34 @@
+app.directive('showDirective', function() {
+  return {
+    restrict: "E",
+    template: "<div>"+
+            "<div id='mainDiv'> </div>" +
+            "<button data-ng-click='append()'>Add</button>" +
+            "</div>",
+    controller: function($http, $scope, $element) {
+
+
+      //get request
+      $http.jsonp('https://archive.org/advancedsearch.php?q=BrothersPast&fl%5B%5D=identifier,title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=5&page=1&output=json&callback=JSON_CALLBACK')
+      .success(function(data) {
+
+          $scope.showList = data;
+          console.log(data);
+          $scope.append = function() {
+          var p = angular.element("<p />");
+          p.text(data.response);
+          $element.find("div").append(p);
+          }
+
+      })
+
+    // function($scope, $element, $attrs) {
+    //             $scope.append = function() {
+    //             var p = angular.element("<p />");
+    //             p.text("Appended");
+    //             $element.find("div").append(p);
+    //         }
+    //       }
+        }
+      }
+});
