2a6403f54dc4f75f7042a810ecfe628d0065618b
Mostafa Habib <MostafaSHabib@gmail.com>
2015-09-16 09:27:33 -0400

    SONGS LIST WORKING

--- js/controllers/showController.js
+++ js/controllers/showController.js
@@ -1,7 +1,5 @@
 //dependency injection for AJAX calls
 app.controller('showController', ['$http', '$scope', function($http, $scope) {
-
-
   //get request
   $http.jsonp('https://archive.org/advancedsearch.php?q=BrothersPast&fl%5B%5D=identifier,title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=50&page=1&output=json&callback=JSON_CALLBACK')
   .then(function(data) {
@@ -13,35 +11,30 @@ app.controller('showController', ['$http', '$scope', function($http, $scope) {
 
 
 app.controller('songController', ['$http', '$scope', function($http, $scope) {
-
-    $scope.go = function(showID) {
-
+    $scope.go = function getSongNow(showID) {
       console.log('https://archive.org/metadata/' + showID + '?callback=JSON_CALLBACK');
-
       $http.jsonp('https://archive.org/metadata/' + showID + '?callback=JSON_CALLBACK')
       .then(function(response) {
-        $scope.songs = response.data.files;
-        // var songs = response.data.files;
+        //var songs = response.data.files;
         //console.log(songs);
-      })
+        // var songs = response.data.files;
+          var songList = [];
+        $.each(response.data.files, function getSong(i, val) {           //iterate and add name and title to variables
+          var fileName = val.name;
+          var songName = val.title;
+          // console.log(songName);
+          // console.log(fileName);
+          var ext = fileName.substr(fileName.lastIndexOf('.') + 1);   //check file type - looks at everything after '.'
+
+            if ((ext === 'ogg' || ext === 'mp3') && songName != undefined) {
+              songList.push({songTitle: songName, songFile: fileName});
+              //songList.push({songTitle: songName, songFile: fileName})
+              //songList += '<li class="songs-li" data-song-title="' + songName + '" data-song-src="' + fileName + '">' + songName + '</li>';
+              //$scope.songs = songList;
+              console.log(songList);
+              $scope.songs = songList
+            }
+          })
+        })
     }
 } ])
-
-        // $.each(response.data.files, function getSong(i, val, songList) {           //iterate and add name and title to variables
-        //   var fileName = val.name;
-        //   var songName = val.title;
-        //   var songList = {};
-        //   var ext = fileName.substr(fileName.lastIndexOf('.') + 1);   //check file type - looks at everything after '.'
-        //
-        //     if ((ext === 'ogg' || ext === 'mp3') && songName != undefined) {
-        //       songList.songTitle = songName;
-        //       songList.songFile = fileName;
-        //       //songList.push({songTitle: songName, songFile: fileName})
-        //       //songList += '<li class="songs-li" data-song-title="' + songName + '" data-song-src="' + fileName + '">' + songName + '</li>';
-        //       //$scope.songs = songList;
-        //       console.log(songList);
-        //       return songList;
-        //       //$scope.songs = "songList"
-        //     }
-        //
-        // })
--- js/directives/getSong.js
+++ js/directives/getSong.js
@@ -1,34 +1,51 @@
+app.directive('getSongClick', function() {
+  return {
+    restrict: "AE",
+    scope: {
+      songs: "@"
+    },
+    template: "<button ng-click='clickMe()'>Test Songs</button>",
+    link: function($scope) {
+            $scope.clickMe = function() {
+                console.log(songs);
+            }
+    }
+  }
+});
+
+
 app.directive('getSongD', function() {
   return {
     restrict: 'E',
     controller: 'songController',
     scope: {
-      songs: "@",
-      songListFinal: "@"
+      songs: "@"
+      //songListFinal: "="
     },
-    link: function(songs, songListFinal){
-      $.each(songs, function getSong(i, songs, songList) {           //iterate and add name and title to variables
-        var fileName = songs.name;
-        var songName = songs.title;
-        var songList = {};
-        var ext = fileName.substr(fileName.lastIndexOf('.') + 1);   //check file type - looks at everything after '.'
-
-          if ((ext === 'ogg' || ext === 'mp3') && songName != undefined) {
-            songList.songTitle = songName;
-            songList.songFile = fileName;
-            //songList.push({songTitle: songName, songFile: fileName})
-            //songList += '<li class="songs-li" data-song-title="' + songName + '" data-song-src="' + fileName + '">' + songName + '</li>';
-            //$scope.songs = songList;
-            console.log(songList);
-            return songList;
-            //$scope.songs = "songList"
-          }
-
-      })
-      console.log("directive ran");
+    link: function(songs){
+      console.log(songs);
+      // $.each(songs, function getSong(i, songs, songList) {           //iterate and add name and title to variables
+      //   var fileName = songs.name;
+      //   var songName = songs.title;
+      //   var songList = {};
+      //   var ext = fileName.substr(fileName.lastIndexOf('.') + 1);   //check file type - looks at everything after '.'
+      //
+      //     if ((ext === 'ogg' || ext === 'mp3') && songName != undefined) {
+      //       songList.songTitle = songName;
+      //       songList.songFile = fileName;
+      //       //songList.push({songTitle: songName, songFile: fileName})
+      //       //songList += '<li class="songs-li" data-song-title="' + songName + '" data-song-src="' + fileName + '">' + songName + '</li>';
+      //       //$scope.songs = songList;
+      //       console.log(songList);
+      //       return songList;
+      //       //$scope.songs = "songList"
+      //     }
+      //
+      // })
+      console.log();
     },
-    templateUrl: "/views/songsPartial.html",
-    // template: '<h1>HELLO<h1>',
+    //templateUrl: "/views/songsPartial.html",
+    template: '<h1>{{ songs }}<h1>',
   };
 });
 
