e0d17807aa4250ddc76c85dab40e2410e1ec3602
Mostafa Habib <MostafaSHabib@gmail.com>
2015-09-17 20:39:40 -0400

    added all BP shows

--- js/controllers/showController.js
+++ js/controllers/showController.js
@@ -1,7 +1,7 @@
 //dependency injection for AJAX calls
 app.controller('showController', ['$http', '$scope', function($http, $scope) {
   //get request
-  $http.jsonp('https://archive.org/advancedsearch.php?q=BrothersPast&fl%5B%5D=identifier,title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=50&page=1&output=json&callback=JSON_CALLBACK')
+  $http.jsonp('https://archive.org/advancedsearch.php?q=BrothersPast&fl%5B%5D=identifier,title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=550&page=1&output=json&callback=JSON_CALLBACK')
   .then(function(data) {
     $scope.shows = data.data.response.docs;
     });
