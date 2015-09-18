412eb005f64722b7f3fca2b74c7742f6593e7fcd
Mostafa Habib <MostafaSHabib@gmail.com>
2015-09-17 23:27:41 -0400

    all bp shows again

--- js/controllers/showController.js
+++ js/controllers/showController.js
@@ -2,7 +2,7 @@
 app.controller('showController', ['$http', '$scope', function($http, $scope) {
   //get request
 
-  $http.jsonp('https://archive.org/advancedsearch.php?q=BrothersPast&fl%5B%5D=year&fl%5B%5D=date&fl%5B%5D=identifier,title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=50&page=1&output=json&callback=JSON_CALLBACK')
+  $http.jsonp('https://archive.org/advancedsearch.php?q=BrothersPast&fl%5B%5D=year&fl%5B%5D=date&fl%5B%5D=identifier,title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=600&page=1&output=json&callback=JSON_CALLBACK')
 
   .then(function(data) {
     $scope.shows = data.data.response.docs;
