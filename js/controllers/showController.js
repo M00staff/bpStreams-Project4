c6fcff05d5d548e6bbe2cd556f2b931361bc2fb7
Mostafa Habib <MostafaSHabib@gmail.com>
2015-09-21 23:26:08 -0400

    end of set loops back to begining deleted all throwaway code

--- js/controllers/showController.js
+++ js/controllers/showController.js
@@ -1,12 +1,11 @@
 //dependency injection for AJAX calls
 app.controller('showController', ['$http', '$scope', function($http, $scope) {
-  //get request
+  //first get request
   $scope.pickYear = function(year) {
   $http.jsonp('https://archive.org/advancedsearch.php?q=BrothersPast,%20year:' +year+ '&fl%5B%5D=year&fl%5B%5D=date&fl%5B%5D=identifier,title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=600&page=1&output=json&callback=JSON_CALLBACK')
 
   .then(function(data) {
     $scope.shows = data.data.response.docs;
-    //console.log(data.data.response.docs);
     });
   }
 } ]);
@@ -16,31 +15,18 @@ app.controller('showController', ['$http', '$scope', function($http, $scope) {
 
 app.controller('songController', ['$http', '$scope', function($http, $scope) {
     $scope.go = function getSongNow(showID) {
-      //console.log('https://archive.org/metadata/' + showID + '?callback=JSON_CALLBACK');
       $http.jsonp('https://archive.org/metadata/' + showID + '?callback=JSON_CALLBACK')
       .then(function(response) {
-        //var songs = response.data.files;
-        //console.log(songs);
-        // var songs = response.data.files;
-          //================================MAYBE PUT THIS INTO A SERVICE inject into another controller and use for footer/playlist
           var songList = [];
         $.each(response.data.files, function getSong(i, val) {           //iterate and add name and title to variables
           var fileName = val.name;
           var songName = val.title;
           var baseUrl = response.data.d1;
-          //console.log(baseUrl);
           var dir = response.data.dir;
-          //console.log(dir);
-          // console.log(songName);
-          // console.log(fileName);
           var ext = fileName.substr(fileName.lastIndexOf('.') + 1);   //check file type - looks at everything after '.'
 
             if ((ext === 'ogg' || ext === 'mp3') && songName != undefined) {
               songList.push({songTitle: songName, songFile: fileName, deeOne: baseUrl, directory: dir, songSource1: 'http://' + baseUrl + dir + '/' + fileName});
-              //songList.push({songTitle: songName, songFile: fileName})
-              //songList += '<li class="songs-li" data-song-title="' + songName + '" data-song-src="' + fileName + '">' + songName + '</li>';
-              //$scope.songs = songList;
-              //console.log(songList);
               $scope.songs = songList;
 
               //================================sorts playlist
@@ -71,16 +57,16 @@ app.controller('getSongCtrl', ['$http', '$scope', function($http, $scope) {
   $('.player').attr('src', songList[index].songSource1);
 
 
-  //console.log(index);
+                                //=========== event listener for next song
   var audio = $('audio');
   var songCount = index;
   var len = songList.length - 1;
   audio[0].addEventListener('ended', function(e){
       songCount++;
-      if(songCount == len){
-          songCount = 0;
-          $('.player-song-title').html(songList[songCount].songTitle);
-          $('.player').attr('src', songList[songCount].songSource1);
+      if(songCount >= len){
+          // songCount = 0;
+          $('.player-song-title').html(songList[0].songTitle);
+          $('.player').attr('src', songList[0].songSource1);
       } else{
         $('.player-song-title').html(songList[songCount].songTitle);
         $('.player').attr('src', songList[songCount].songSource1);
