9d64cef7459897c5b145723db590b9e0c05b7617
M00staff <Mostafa@nete.com>
2016-03-21 15:38:27 -0400

    first controller logic moved into showFactory, and working

--- js/factories/showFactory.js
+++ js/factories/showFactory.js
@@ -2,32 +2,7 @@ app.factory('showFactory', ['$http', function( $http ) {
   //first get request
   return {showYear: function(year, row) {
 
-    $http.jsonp('https://archive.org/advancedsearch.php?q=BrothersPast,%20year:' +year+ '&fl%5B%5D=year&fl%5B%5D=date&fl%5B%5D=identifier,title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=' +row+ '&page=1&output=json&callback=JSON_CALLBACK')
-
-    .then(function(data) {
-      console.log("factory ran");
-      console.log(year);
-      console.log(row);
-      // console.log(data.data.response.docs);
-      return data.data.response.docs;
-      });
+    return $http.jsonp('https://archive.org/advancedsearch.php?q=BrothersPast,%20year:' +year+ '&fl%5B%5D=year&fl%5B%5D=date&fl%5B%5D=identifier,title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=' +row+ '&page=1&output=json&callback=JSON_CALLBACK')
     }
-
   }
-
 } ]);
-
-
-// (function () {
-//   var yearFactory = function( year, row ) {
-//     $http.jsonp('https://archive.org/advancedsearch.php?q=BrothersPast,%20year:' +year+ '&fl%5B%5D=year&fl%5B%5D=date&fl%5B%5D=identifier,title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=' +row+ '&page=1&output=json&callback=JSON_CALLBACK')
-//
-//     .then(function(data) {
-//       return data.data.response.docs;
-//       });
-//   };
-//
-//
-//     angular.module('bpStreams').factory('yearFactory', yearFactory);
-//
-// })
