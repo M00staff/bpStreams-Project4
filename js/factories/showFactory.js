89b12c6f8f3626f6375f16762f91f8727cc6dec6
M00staff <Mostafa@nete.com>
2016-03-21 17:57:27 -0400

    all logic moved to factories, controllers now skinny

--- js/factories/showFactory.js
+++ js/factories/showFactory.js
@@ -6,3 +6,72 @@ app.factory('showFactory', ['$http', function( $http ) {
     }
   }
 } ]);
+
+
+app.factory('songFactory', ['$http', function( $http ) {
+  return {
+    showPick: function getSongNow(showID) {
+      return $http.jsonp('https://archive.org/metadata/' + showID + '?callback=JSON_CALLBACK')
+      .then(function(response) {
+          var songList = [];
+        $.each(response.data.files, function getSong(i, val) {           //iterate and add name and title to variables
+          var fileName = val.name;
+          var songName = val.title;
+          var baseUrl = response.data.d1;
+          var dir = response.data.dir;
+          var ext = fileName.substr(fileName.lastIndexOf('.') + 1);   //check file type - looks at everything after '.'
+
+            if ((ext === 'ogg' || ext === 'mp3') && songName != undefined) {
+              songList.push({songTitle: songName, songFile: fileName, deeOne: baseUrl, directory: dir, songSource1: 'http://' + baseUrl + dir + '/' + fileName});
+              //$scope.songs = songList;
+
+              //================================sorts playlist
+              songList.sort(function (a, b) {
+                if (a.songFile > b.songFile) {
+                  return 1;
+                }
+                if (a.songFile < b.songFile) {
+                  return -1;
+                }
+                return 0;
+              })
+
+            }
+        })
+          return songList;
+      })
+    }
+  }
+}]);
+
+
+app.factory('playFactory', ['$http', function($http) {
+  return {
+    songPick: function(title, file, d1, dir, songList, index) {
+    var songSrc = {title: title, source: 'http://' + d1 + dir + '/' + file};
+
+
+    //$scope.songSource = songSrc;
+    $('.player-song-title').html(songSrc.title);          //actual changing of audio source
+    $('.player').attr('src', songList[index].songSource1);
+
+
+                                  //=========== event listener for next song
+    var audio = $('audio');
+    var songCount = index;
+    var len = songList.length - 1;
+    audio[0].addEventListener('ended', function(e){
+        songCount++;
+        if(songCount >= len){
+            // songCount = 0;
+            $('.player-song-title').html(songList[0].songTitle);
+            $('.player').attr('src', songList[0].songSource1);
+        } else{
+          $('.player-song-title').html(songList[songCount].songTitle);
+          $('.player').attr('src', songList[songCount].songSource1);
+        }
+      });
+//return songSrc;
+    }
+  }
+}])
